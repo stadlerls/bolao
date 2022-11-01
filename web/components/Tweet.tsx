85c91e03075f0561527d0c513865f7c722db8645
stadlerls <leonardo_stadler@yahoo.com.br>
2022-11-01 00:17:04 -0300

    Commit inicial
    Configurando projeto

---
+++ web/components/Tweet.tsx
@@ -0,0 +1,8 @@
+export function Tweet() {
+    return(
+        <div>
+            <h1>Tweet</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit aut a, tempore id quasi perferendis illo non perspiciatis, accusantium odit iste maiores, hic aperiam aliquid inventore accusamus repellat. Dolor, quae!</p>
+        </div>
+    )
+}
